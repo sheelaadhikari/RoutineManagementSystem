e47e88754e12ff05abdf7618eba59ad5b8bcc4a4
sheelaadhikari <adhikarisheela1000@gmail.com>
2024-02-06 13:28:17 +0545

    added server fille

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,3 +1,5 @@
 const express = require("express");
 const app = express();
-app.listen(8001, () => console.log("Backend is running in port 8001"));
+app.listen(8001, () =>
+  console.log("Backend is running in port 8001 and client in 3000")
+);
