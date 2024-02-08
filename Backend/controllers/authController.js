180b2b3034cec27eb2516fe94d93f92794c87c25
sheelaadhikari <adhikarisheela1000@gmail.com>
2024-02-08 11:12:50 +0545

    added register controller

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,5 +1,11 @@
+export const registerController = async (req, res) => {
+  try {
+    console.log("this is register controller");
+  } catch {}
+};
+
 export const loginController = async (req, res) => {
   try {
-    console.log("hello users");
+    console.log("This is login Controller");
   } catch {}
 };
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { loginController } from "../controllers/authController.js";
+import {
+  loginController,
+  registerController,
+} from "../controllers/authController.js";
 const router = express.Router();
 
 //routing
 router.post("/login", loginController);
+router.post("/register", registerController);
 // hi
 export default router;
