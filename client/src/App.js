4a6ea22a87f5fb15179fa427f57a78c240349bc1
peace011 <gpuja7044@gmail.com>
2024-02-13 17:12:16 +0545

    login design

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
-/* eslint-disable no-unused-vars */
 import logo from "./logo.svg";
 import "./App.css";
+import style from './style.css';
+import Login from "./Pages/LoginPage/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Welcome to Routine Management System.
-        this is puja and sheels,
-        We will make it top ..
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
